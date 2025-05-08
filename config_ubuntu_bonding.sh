dc921cc2dbca0e57a719975eba9a7f974a0bd1de
HsiehBro <186895009+HsiehBro@users.noreply.github.com>
2025-05-09 00:46:28 +0800

    Create config_ubuntu_bonding.sh

---
+++ config_ubuntu_bonding.sh
@@ -0,0 +1,29 @@
+#!/bin/env bash
+set -eu
+
+BONDING_NAME=bond0
+
+sudo cat > "/etc/netplan/${BONDING_NAME}.yaml" << EOF
+network:
+  version: 2
+  renderer: networkd
+  ethernets:
+    eth1: {}
+    eth2: {}
+  bonds:
+    "${BONDING_NAME}":
+      dhcp4: no
+      addresses: [10.0.0.100/24]
+      interfaces:
+        - eth1
+        - eth2
+      parameters:
+        mode: active-backup
+        primary: eth2
+        mii-monitor-interval: 100
+      nameservers:
+        addresses: [223.5.5.5]
+EOF
+
+sudo netplan apply
+sudo cat "/proc/net/bonding/${BONDING_NAME}"
