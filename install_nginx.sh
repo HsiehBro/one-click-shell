320b5f45aa5e8198b945f489ff0badd775bb3eda
HsiehBro <186895009+HsiehBro@users.noreply.github.com>
2025-05-25 00:23:08 +0800

    Update install_nginx.sh

--- install_nginx.sh
+++ install_nginx.sh
@@ -2,27 +2,44 @@
 
 set -u
 
+yum install -y wget gcc pcre-devel openssl-devel
+
 DOWNLOAD_LINK="https://nginx.org/download/nginx-1.28.0.tar.gz"
 DOWNLOAD_DIR="/usr/local/src"
 NGINX_NAME=$(basename "${DOWNLOAD_LINK}" .tar.gz)
+INSTALL_DIR="/opt/${NGINX_NAME}"
 
-cd "${DOWNLOAD_DIR}"
-if [ ! -e "${NGINX_NAME}.tar.gz" ];then
+if [ ! -e "${DOWNLOAD_DIR}/${NGINX_NAME}.tar.gz" ];then
   wget "${DOWNLOAD_LINK}" -P "${DOWNLOAD_DIR}" && echo "nginx download sucessful..." || { echo "nginx download failed!!!";exit 400; };
 fi
-if [ ! -e "${NGINX_NAME}" ];then
+if [ ! -e "${DOWNLOAD_DIR}/${NGINX_NAME}" ];then
   tar -xf "${NGINX_NAME}.tar.gz";
 fi
 
-cd ${NGINX_NAME}
-yum install -y gcc pcre-devel openssl-devel
-./configure --prefix="/opt/${NGINX_NAME}" --with-http_ssl_module
+./configure --prefix="${INSTALL_DIR}" --with-http_ssl_module
 make -j "$(grep -c processor /proc/cpuinfo)" && make install
 
 if [ $? -ne 0 ];then
   rm -rf "/opt/${NGINX_NAME}"
   rm -rf "${DOWNLOAD_DIR}/${NGINX_NAME}"
-else
-  echo "nginx was built and installed successfully ~"
+  exit 400
 fi
-  
+
+cat > "/etc/systemd/system/nginx.service" <<EOF
+[Unit]
+Description=The NGINX HTTP and reverse proxy server
+After=network.target
+
+[Service]
+Type=forking
+ExecStart="/opt/${NGINX_NAME}"/sbin/nginx
+ExecReload="/opt/${NGINX_NAME}"/sbin/nginx -s reload
+ExecStop="/opt/${NGINX_NAME}"/sbin/nginx -s quit
+PIDFile="/opt/${NGINX_NAME}"/logs/nginx.pid
+Restart=on-failure
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+echo -e "\e[30;31sucess\e[0m install"
